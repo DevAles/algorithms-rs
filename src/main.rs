6a091e51dd31c274803fb9d187bb98a1f9b63c28
DevAles <devalesoficial@gmail.com name = DevAles>
2023-03-14 19:49:54 -0300

    Code: Make bubble_sort function with the implemented algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn bubble_sort(arr: &mut [i32]) {
+    for i in 0..arr.len() - 1 {
+        for j in 0..arr.len() - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1)
+            }
+        }
+    }
+
+    println!("{:?}", arr);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut arr = [1, 3, 2, 9, 4, 92, 43, 58, 21];
+    bubble_sort(&mut arr);
 }
